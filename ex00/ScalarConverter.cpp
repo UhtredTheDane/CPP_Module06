7df00b41747ed2e67bb4fa12926e448a03cfae25
agengemb <gengembre.alexis@gmail.com>
2023-12-16 17:41:51 +0100

    ex00 normalement fini

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//std::stringstream
-
 #include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter(void)
@@ -35,10 +33,10 @@ ScalarConverter& ScalarConverter::operator=(ScalarConverter const& toAffect)
 	return (*this);
 }
 
-static void convertInChar(long double first_value, bool nan, bool infp, bool infm)
+static void convertInChar(long double first_value, int symbol)
 {
 	std::cout << "char: ";
-	if (first_value < 0 || first_value > 127 || nan || infp || infm)
+	if (first_value < 0 || first_value > 127 || symbol > 1)
 		std::cout << "impossible";
 	else
 	{
@@ -50,78 +48,92 @@ static void convertInChar(long double first_value, bool nan, bool infp, bool inf
 	std::cout << std::endl;
 }
 
-static void convertInInt(long double first_value, bool nan, bool infp, bool infm)
+static void convertInInt(long double first_value, int symbol)
 {
 	std::cout << "int: ";
-	if (first_value < INT_MIN || first_value > INT_MAX || nan || infp || infm)
+	if (first_value < INT_MIN || first_value > INT_MAX || symbol > 1)
 		std::cout << "impossible";
 	else
 		std::cout << static_cast<int>(first_value);
 	std::cout << std::endl;
 }
 
-
-void convertInFloat(long double first_value, bool nan, bool infp, bool infm)
+void convertInFloat(long double first_value, int symbol)
 {
 	std::cout << "float: ";
 	if (first_value < FLT_MAX * -1 || first_value > FLT_MAX)
 		std::cout << "impossible";
-	else if(nan)
+	else if(symbol == 2)
 		std::cout << "nanf";
-	else if (infp)
+	else if (symbol == 3)
 		std::cout << "+inff";
-	else if (infm)
+	else if (symbol == 4)
 		std::cout << "-inff";
 	else
-		std::cout << static_cast<float>(first_value) << "f";
+	{
+		std::cout << static_cast<float>(first_value);
+		if (symbol == 1 && first_value < 1000000)
+			std::cout << ".0";
+		std::cout << "f";
+	}
 	std::cout << std::endl;
 }
 
-static void convertInDouble(long double first_value, bool nan, bool infp, bool infm)
+static void convertInDouble(long double first_value, int symbol)
 {
-	double double_value;
-
 	std::cout << "double: ";
 	if (first_value < DBL_MAX * -1 || first_value > DBL_MAX)
 		std::cout << "impossible";
-	else if(nan)
+	else if(symbol == 2)
 		std::cout << "nan";
-	else if (infp)
+	else if (symbol == 3)
 		std::cout << "+inf";
-	else if (infm)
+	else if (symbol == 4)
 		std::cout << "-inf";
 	else
+	{
 		std::cout << static_cast<double>(first_value);
+		if (symbol == 1 && first_value < 1000000)
+			std::cout << ".0";
+	}
 	std::cout << std::endl;
+}
+
+int checkSymbol(std::string value)
+{
+	if (value.compare("nan") == 0 || value.compare("nanf") == 0)
+		return (2);
+	else if (value.compare("+inf") == 0)
+		return (3);
+	else if (value.compare("-inf") == 0)
+		return (4);
+	return (0);
 }
 
 void ScalarConverter::convert(std::string value)
 {
 	std::stringstream test;
+	std::string remainder;
 	long double first_value;
-	bool nan;
-	bool infp;
-	bool infm;
+	int symbol;
 
 	first_value = 0;
-	nan = false;
-	infp = false;
-	infm = false;
-	if (value.compare("nan") == 0)
-		nan = true;
-	else if (value.compare("+inf") == 0)
-		infp = true;
-	else if (value.compare("-inf") == 0)
-		infm = true;
-	else
-	{	
-		test << value;	
+	symbol = checkSymbol(value);
+	if (!symbol)
+	{
+		if (value.find('.'))
+			symbol = 1;
+		test << value;
 		test >> first_value;
-		if (!test.eof())
+		test >> remainder;
+		if (!test.eof() || (remainder.size() > 0 && remainder.compare("f")))
+		{
 			std::cout << "Bad input" << std::endl;
+			return;
+		}
 	}
-	convertInChar(first_value, nan, infp, infm);
-	convertInInt(first_value, nan, infp, infm);
-	convertInFloat(first_value, nan, infp, infm);
-	convertInDouble(first_value, nan, infp, infm);
+	convertInChar(first_value, symbol);
+	convertInInt(first_value, symbol);
+	convertInFloat(first_value, symbol);
+	convertInDouble(first_value, symbol);
 }
